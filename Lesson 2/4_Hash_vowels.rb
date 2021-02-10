efa66f01b9813817102bf99461a4f340146e7389
Pavel-Zyryanov <softradmov@yandex.ru>
2021-02-10 22:27:41 +0500

    With addition

--- Lesson 2/4_Hash_vowels.rb
+++ Lesson 2/4_Hash_vowels.rb
@@ -4,9 +4,9 @@ alphabet = ('a'..'z').to_a
 vowels = ['a' ,'e' ,'i' ,'o' ,'u' ,'y']
 
 #Просмотр массива алфавита с индексами элементов, занесение в hash значения индексов гласных букв
-alphabet.each_with_index do |letter, index|
+alphabet.each.with_index(1) do |letter, index|
    if vowels.include?(letter)
-     hash_vowels[letter] = index + 1
+     hash_vowels[letter] = index
    end
 end
 
