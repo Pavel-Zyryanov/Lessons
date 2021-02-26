4a8f8394c835f78430274b2e5904753d9aead8ac
Pavel-Zyryanov <softradmov@yandex.ru>
2021-02-26 19:50:55 +0500

    Lesson 4

---
+++ Lesson 4/main.rb
@@ -0,0 +1,12 @@
+require_relative 'menu'
+require_relative 'car'
+require_relative 'cargo_car'
+require_relative 'passenger_car'
+require_relative 'station'
+require_relative 'route'
+require_relative 'train'
+require_relative 'cargo_train'
+require_relative 'passenger_train'
+
+test = Menu.new
+test.start_menu
