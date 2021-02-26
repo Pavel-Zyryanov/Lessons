09f2a2f7a406d76b4dd74113b274cc152f60f587
Pavel-Zyryanov <softradmov@yandex.ru>
2021-02-26 19:47:05 +0500

    Lesson 4

---
+++ Lesson 4/station.rb
@@ -0,0 +1,35 @@
+class Station
+  attr_reader :name, :trains
+
+  def initialize(name)
+    @name = name
+    @trains = []
+  end
+
+  def get_train(train)
+    @trains << train
+  end
+
+  def show_trains
+    puts "На станции #{name} находится #{trains.size} поезд(ов)"
+    trains.each do |train|
+      puts "Поезд № #{train.number}"
+    end
+  end
+
+  def show_trains_with_type(type)
+    index_type = 0
+    puts"На станции #{name} находится всего #{trains.size} поезд(-ов, -а) из них #{type} типа:"
+    trains.each do |train|
+    if train.type == type
+      index_type += 1
+      puts "Поезд № #{train.number}, тип: #{train.type}."
+    end
+    end
+    puts "Всего поездов #{type} типа: #{index_type} "
+  end
+
+  def depart_train(train)
+    trains.delete(train)
+  end
+end
