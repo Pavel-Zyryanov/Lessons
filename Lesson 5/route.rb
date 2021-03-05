0e1eb5599361251c3cd5ab015e98544afe6e9cd9
Pavel-Zyryanov <softradmov@yandex.ru>
2021-03-05 22:25:03 +0500

    Lesson 5

---
+++ Lesson 5/route.rb
@@ -0,0 +1,31 @@
+class Route
+  attr_reader :stations
+  include InstanceCounter
+
+  def initialize(first_station, last_station)
+    @stations = [first_station, last_station]
+    register_instance
+  end
+
+  def name
+    @name = stations.first.name + ' - ' + stations.last.name
+  end
+
+  def add_station(station)
+    @stations.insert(-2, station)
+  end
+
+  def delete_station(station)
+    if [stations.first, stations.last].include?(station)
+      puts "Нельзя удалять первую и последнюю станцию маршрута."
+    else
+      stations.delete(station)
+    end
+  end
+
+  def show_all_station
+    stations.each do |station|
+      puts station.name
+      end
+  end
+end
