af1cfb1f90169b75335484d7bac59fb8ccb8f9a7
Pavel-Zyryanov <softradmov@yandex.ru>
2021-03-18 20:19:41 +0500

    Lesson 7 with addition_3

--- Lesson 7/cargo_car.rb
+++ Lesson 7/cargo_car.rb
@@ -1,22 +1,11 @@
 class CargoCar < Car
-  attr_reader :free_volume
 
-  def initialize(id, volume)
-    super(id)
+  def initialize(id, place)
+    super(id, place)
     @type = 'Cargo'
-    @volume = volume
-    @free_volume = volume
   end
 
-  def take_place(volume)
-    @free_volume -= volume if (@free_volume - volume) >= 0
-  end
-
-  def occupied_volume
-    @volume - @free_volume
-  end
-
-  def free_volume
-    @free_volume
+  def take_place(place)
+    @occupied_place += place if (@place - @occupied_place - place) >= 0
   end
 end
