f3ae105ccfbcff67cbbc8377106ede6365318ac5
Pavel-Zyryanov <softradmov@yandex.ru>
2021-03-18 20:19:07 +0500

    Lesson 7 with addition_3

--- Lesson 7/car.rb
+++ Lesson 7/car.rb
@@ -1,5 +1,5 @@
 class Car
-  attr_reader :id , :type
+  attr_reader :id , :type, :place, :occupied_place
   attr_accessor :train
   include Manufacturer
   include Valid
@@ -7,10 +7,16 @@ class Car
 
   CAR_ID_FORMAT = /[0-9]{2}-[0-9]{3}$/i
 
-  def initialize(id)
+  def initialize(id, place)
     @id = id
     validate!
+    @place = place
+    @occupied_place = 0
     @@cars[id] = self
+  end
+
+  def free_place
+    @place - @occupied_place
   end
 
   protected
