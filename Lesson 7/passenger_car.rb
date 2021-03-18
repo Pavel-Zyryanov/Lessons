1871e5ddeb277b0dd4761f8da8bb1b4f6a7cefb4
Pavel-Zyryanov <softradmov@yandex.ru>
2021-03-18 20:20:00 +0500

    Lesson 7 with addition_3

--- Lesson 7/passenger_car.rb
+++ Lesson 7/passenger_car.rb
@@ -1,22 +1,11 @@
 class PassengerCar < Car
-  attr_reader :number_free_seats
 
-  def initialize(id, number_seats)
-    super(id)
+  def initialize(id, place)
+    super(id, place)
     @type = 'Passenger'
-    @number_seats = number_seats
-    @number_free_seats = number_seats
   end
 
   def take_place
-    @number_free_seats -= 1 if @number_free_seats > 0
-  end
-
-  def occupied_seats
-    @number_seats - @number_free_seats
-  end
-
-  def free_seats
-    @number_free_seats
+    @occupied_place += 1 if @place - @occupied_place > 0
   end
 end
