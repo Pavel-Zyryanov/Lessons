bf23bde748300c12923a3b684d6c46ba5e338e68
Pavel-Zyryanov <softradmov@yandex.ru>
2021-03-22 00:23:40 +0500

    Lesson 8

---
+++ Lesson 8/car.rb
@@ -0,0 +1,41 @@
+class Car
+  attr_reader :id, :type, :place, :occupied_place
+  attr_accessor :train
+
+  include Manufacturer
+  include Valid
+  @@cars = {}
+
+  CAR_ID_FORMAT = /[0-9]{2}-[0-9]{3}$/i
+
+  def initialize(id, place)
+    @id = id
+    validate!
+    @place = place
+    @occupied_place = 0
+    @@cars[id] = self
+  end
+
+  def free_place
+    @place - @occupied_place
+  end
+
+  def take_place(place)
+    @occupied_place += place if free_place >= place
+  end
+
+  protected
+
+  def validate!
+    validate_car_id
+    validate_car_exist
+  end
+
+  def validate_car_id
+    raise 'Неверный формат ID вагона' if @id !~ CAR_ID_FORMAT
+  end
+
+  def validate_car_exist
+    raise 'Вагон с таким ID уже есть' unless @@cars[id].nil?
+  end
+end
