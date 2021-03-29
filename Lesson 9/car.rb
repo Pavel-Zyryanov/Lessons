f8bd9cfaa6b6eaba51ae67fd14a7a17d0da7d37e
Pavel-Zyryanov <softradmov@yandex.ru>
2021-03-29 23:15:26 +0500

    Lesson 9

---
+++ Lesson 9/car.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class Car
+  attr_reader :id, :type, :place, :occupied_place
+  attr_accessor :train
+
+  include Manufacturer
+  include Validation
+  @@cars = {}
+
+  CAR_ID_FORMAT = /[0-9]{2}-[0-9]{3}$/i.freeze
+
+  validate :id, presence: true, format: CAR_ID_FORMAT
+
+  def initialize(id, place)
+    @id = id
+    validate!
+    validate_car_exist
+    @place = place
+    @occupied_place = 0
+    @@cars[id] = self
+  end
+
+  def free_place
+    @place - @occupied_place
+  end
+
+  def take_place(place)
+    @occupied_place += place if free_place >= place
+  end
+
+  protected
+
+  def validate_car_exist
+    raise 'Вагон с таким ID уже есть' unless @@cars[id].nil?
+  end
+end
