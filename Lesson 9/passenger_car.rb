80d27c97aee8ddd457aa345f85056772655f0314
Pavel-Zyryanov <softradmov@yandex.ru>
2021-03-29 23:18:16 +0500

    Lesson 9

---
+++ Lesson 9/passenger_car.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class PassengerCar < Car
+  def initialize(id, place)
+    super(id, place)
+    @type = 'Passenger'
+  end
+
+  def take_place
+    super(1)
+  end
+end
