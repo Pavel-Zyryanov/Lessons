6751918d5d0ae89f31b283d89e8a8b90943be063
Pavel-Zyryanov <softradmov@yandex.ru>
2021-03-31 01:57:30 +0500

    Lesson 9 with addition

--- Lesson 9/passenger_train.rb
+++ Lesson 9/passenger_train.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class PassengerTrain < Train
+  validate :number, :type, String
+  validate :number, :presence
+  validate :number, :format, NUMBER_FORMAT
+
   def initialize(number)
     super
     @type = 'Passenger'
