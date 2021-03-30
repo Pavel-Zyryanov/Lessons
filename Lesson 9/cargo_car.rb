cc34b7ae06409a7717c67606a945d390b8241e62
Pavel-Zyryanov <softradmov@yandex.ru>
2021-03-31 01:55:24 +0500

    Lesson 9 with addition

--- Lesson 9/cargo_car.rb
+++ Lesson 9/cargo_car.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class CargoCar < Car
+  validate :id, :presence
+  validate :id, :format, CAR_ID_FORMAT
+  validate :id, :type, String
+
   def initialize(id, place)
     super(id, place)
     @type = 'Cargo'
